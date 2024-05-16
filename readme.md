249c60c59c8165c5a1779ebca95f56218fcdcf9f
keteb <ketebmehdi2@gmail.com>
2024-05-16 23:53:58 +0200

    add plot

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Project time serie ENSAE
-- On utilise la base de donnée suivante de Janvier 1990 à Décembre 2018 afin de ne pas prendre en compte la période du covid-19
-- Base de l'INSEE [ici](https://www.insee.fr/fr/statistiques/serie/010767832#Telechargement): Indice CVS-CJO de la production industrielle (base 100 en 2021) - Industrie pharmaceutique (NAF rév. 2, niveau division, poste 21) Identifiant 010767832
+- On utilise la base de donnée suivante de Janvier 1990 à Janvier 2018 afin de ne pas prendre en compte la période du covid-19
+- Base de l'INSEE [ici](https://www.insee.fr/fr/statistiques/serie/010767631#Graphique): ndice CVS-CJO de la production industrielle (base 100 en 2021) - Fabrication de produits laitiers (NAF rév. 2, niveau groupe, poste 10.5) Identifiant 010767631
+
--- script_project.R
+++ script_project.R
@@ -2,12 +2,27 @@ require(zoo) #format de serie temporelle pratique et facile d'utilisation (mais 
 require(tseries)
 
 
-xm <- rev(zoo(serie_lt$index))
-plot(xm)
+xm <- rev(zoo(serie_lt$index)) # création de l'objet Zoo
+length(xm) # le nombre d'observation de la série 
 
 
-#x  <- data.frame(data = ts(serie$index))
-#plot(x)
+dates <- paste0(serie_lt$time, "-01")
+dates <- as.Date(rev(dates), format="%Y-%m-%d")
+
+
+
+plot(dates, xm, type = "l", col="blue", main="Indice de la production industrielle", xlab="Dates", ylab="Index (Base 100)", xaxt="n")
+
+# Sélectionner seulement quelques dates à afficher
+vecteur <- round(seq(from=1, to=337, length.out=10))
+
+dates_to_display <- dates[vecteur]
+labels_to_display <- format(dates_to_display, "%Y-%m")
+axis(1, at=dates_to_display, labels=FALSE)
+text(x = dates_to_display, y = par("usr")[3] - 3, labels = labels_to_display, srt = 45, adj = 1, xpd = TRUE, cex = 0.7)
+grid()
+
+
 
 acf(xm) # on affiche les auto corrélation 
 
@@ -39,7 +54,7 @@ print(adf_result)
 
 
 
-y <- (xm_diff - mean(xm_diff))/ (var(xm_diff)*1/2) #centrer la série
+y <- (xm_diff - mean(xm_diff)) #centrer la série
 
 plot(y)
 acf(y,20)
@@ -67,16 +82,73 @@ Qtests(arima302$residuals, 20, 7)
 # vérification de tous les models 
 
 
-modelchoice <- function(p,q,data= y, k=20){
+p = 6 ; q = 1
+
+
+
+## fonction pour estimer un arima et en v´erifier l’ajustement et la validit´e
+
+
+
+signif <- function(estim){ #fonction de test des significations individuelles des coefficients
+  coef <- estim$coef
+  se <- sqrt(diag(estim$var.coef))
+  t <- coef/se
+  pval <- (1-pnorm(abs(t)))*2
+  return(rbind(coef,se,pval))
+}
+
+## fonction pour estimer un arima et en verifier l'ajustement et la validite
+modelchoice <- function(p,q,data=y, k=20){
   estim <- try(arima(data, c(p,0,q),optim.control=list(maxit=20000)))
   if (class(estim)=="try-error") return(c("p"=p,"q"=q,"arsignif"=NA,"masignif"=NA,"resnocorr"=NA, "ok"=NA))
   arsignif <- if (p==0) NA else signif(estim)[3,p]<=0.05
   masignif <- if (q==0) NA else signif(estim)[3,p+q]<=0.05
-  resnocorr <- sum(Qtests(estim$residuals,24,length(estim$coef)-1)[,2]<=0.05,na.rm=T)==0
+  resnocorr <- sum(Qtests(estim$residuals,20,length(estim$coef)-1)[,2]<=0.05,na.rm=T)==0
   checks <- c(arsignif,masignif,resnocorr)
   ok <- as.numeric(sum(checks,na.rm=T)==(3-sum(is.na(checks))))
   return(c("p"=p,"q"=q,"arsignif"=arsignif,"masignif"=masignif,"resnocorr"=resnocorr,"ok"=ok))
 }
+
+## fonction pour estimer et verifier tous les arima(p,q) avec p<=pmax et q<=max
+armamodelchoice <- function(pmax,qmax){
+  pqs <- expand.grid(0:pmax,0:qmax)
+  t(apply(matrix(1:dim(pqs)[1]),1,function(row) {
+    p <- pqs[row,1]; q <- pqs[row,2]
+    cat(paste0("Computing ARMA(",p,",",q,") \n"))
+    modelchoice(p,q)
+  }))
+}
+
+armamodels <- armamodelchoice(pmax = 6,qmax = 1)
+
+
+
+selec <- armamodels[armamodels[,"ok"]==1&!is.na(armamodels[,"ok"]),]
+selec
+
+##
+
+
+
+pqs <- apply(selec,1,function(row) list("p"=as.numeric(row[1]),"q"=as.numeric(row[2]))) #cr´ee
+#une liste des ordres p et q des mod`eles candidats
+names(pqs) <- paste0("arma(",selec[,1],",",selec[,2],")") #renomme les ´el´ements de la liste
+models <- lapply(pqs, function(pq) arima(y,c(pq[["p"]],0,pq[["q"]]))) #cr´ee une liste des mod`eles
+vapply(models, FUN.VALUE=numeric(2), function(m) c("AIC"=AIC(m),"BIC"=BIC(m))) #calcule les AIC et
+#BIC des mod`eles candidats
+
+
+T <- length(xm)
+
+
+
+
+
+
+
+
+
 
 
 
